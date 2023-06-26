88efcbc2afc32508801c7059ef8216a479d7bb09
tahirZmn <tahir.zaman@errecompany.org>
2023-06-26 15:15:19 +0200

    exercie on modularity

---
+++ cmakepractice01/source/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "dog.h"
+#include "operations.h"
+
+int main(){
+    
+    std::cout<< "main function () "<<std::endl;
+    Dog dog1("Flitzy");
+     dog1.print_info();
+    
+    
+ return 0;
+} 
